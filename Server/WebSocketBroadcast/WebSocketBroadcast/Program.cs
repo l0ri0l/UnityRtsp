6857ca5e39adc63c503f7c3fcc74481823649889
roger130796@gmail.com <roger130796@gmail.com>
2022-11-24 10:51:40 +0300

    added serber project and install web socket lib

---
+++ Server/WebSocketBroadcast/WebSocketBroadcast/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebSocketBroadcast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
